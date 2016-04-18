c90da8ea180370410d1f5d7834774e281241fa2d
Luke Clark <contact@lukejclark.com>
2016-04-18 13:04:55 +1000

    auto add admin user if none exists

---
+++ updates/0.0.1-admins.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a default user for the cp
+ */
+
+exports.create = {
+  User: [
+    { 'name': 'Luke Clark', email: 'luke@lukeclark.com.au', password: 'admin', isAdmin: true }
+  ]
+}
